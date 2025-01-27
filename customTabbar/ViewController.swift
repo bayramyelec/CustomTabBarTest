269a2e9235573db22933f80281465ba405413d5a
Bayram Yelec <bayrammyelec@gmail.com>
2025-01-27 17:37:28 +0300

    Initial Commit

---
+++ customTabbar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  customTabbar
+//
+//  Created by Bayram Yeleç on 27.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
