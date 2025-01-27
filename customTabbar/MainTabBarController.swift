4e8788fbede7a1991dd39777041a6452543b9819
Bayram Yelec <bayrammyelec@gmail.com>
2025-01-27 17:45:14 +0300

    update

---
+++ customTabbar/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  customTabbar
+//
+//  Created by Bayram Yeleç on 27.01.2025.
+//
+
+import UIKit
+
+class MainTabBarController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
